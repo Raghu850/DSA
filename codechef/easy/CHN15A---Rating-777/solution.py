6ddffb88af7e15bd05f86e507e56baac7626508e
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-07-30 02:29:07 +0530

    ✅ CHN15A - Rating 777 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-29

--- codechef/easy/CHN15A---Rating-777/solution.py
+++ codechef/easy/CHN15A---Rating-777/solution.py
@@ -1,10 +1,11 @@
 # cook your dish here
-n=int(input())
-for i in range(n):
-    a,b=map(int,input().split())
-    if a<b:
-        print("<")
-    elif a>b:
-        print(">")
-    else:
-        print("=")
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    arr=list(map(int,input().split()))
+    ans=0
+    for i in arr:
+        i+=k
+        if i%7==0:
+            ans+=1
+    print(ans)
