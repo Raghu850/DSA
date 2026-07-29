2fb911009e395bbd267e6901ebe533d7b4453e01
Adabala Raghu Ram <raghuram829729@gmail.com>
2026-07-29 09:30:25 +0530

    ✅ Insertion Sort | Easy | Python | Runtime: N/A | Memory: N/A | 2026-07-29

---
+++ gfg/easy/insertion-sort/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def insertionSort(self, arr):
+        # code here
+        for i in range(len(arr)):
+            j=i
+            while j>0 and arr[j-1]>arr[j]:
+                arr[j-1],arr[j]=arr[j],arr[j-1]
+                j-=1
+            
